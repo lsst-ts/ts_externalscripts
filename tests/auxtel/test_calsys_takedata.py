1c32b2b28478c5725680beb84a88b86a4f359854
Russell Owen <rowen@uw.edu>
2019-12-16 14:56:22 -0800

    Update unit tests to use asynctest.

--- tests/auxtel/test_calsys_takedata.py
+++ tests/auxtel/test_calsys_takedata.py
@@ -1,7 +1,7 @@
-import asyncio
 import logging
 import unittest
 
+import asynctest
 import numpy as np
 import yaml
 
@@ -24,23 +24,21 @@ class Harness:
         self.script = CalSysTakeData(index=self.index)
 
 
-class TestCalSysTakeData(unittest.TestCase):
+class TestCalSysTakeData(asynctest.TestCase):
     def setUp(self):
         salobj.test_utils.set_random_lsst_dds_domain()
 
-    def test_configure(self):
+    async def test_configure(self):
         index = next(index_gen)
 
-        async def doit():
-            script = CalSysTakeData(index=index)
-            try:
-                async def run_configure(**kwargs):
-                    script.set_state(salobj.Script.ScriptState.UNCONFIGURED)
-                    config_data = script.cmd_configure.DataType()
-                    if kwargs:
-                        config_data.config = yaml.safe_dump(kwargs)
-                    await script.do_configure(config_data)
-            finally:
-                await script.close()
+        async with CalSysTakeData(index=index) as script:
+            async def run_configure(**kwargs):
+                script.set_state(salobj.Script.ScriptState.UNCONFIGURED)
+                config_data = script.cmd_configure.DataType()
+                if kwargs:
+                    config_data.config = yaml.safe_dump(kwargs)
+                await script.do_configure(config_data)
 
-        asyncio.get_event_loop().run_until_complete(doit())
+
+if __name__ == "__main__":
+    unittest.main()
