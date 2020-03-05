396641c4a8d005597328700f05efbe2f3cf280d8
Patrick Ingraham <pingraham@lsst.org>
2020-03-05 18:29:13 +0000

    remove _old file that was there to resolve conflicts (+37 squashed commits) Squashed commits: [bd102a5] adding file that I think will conflict with most recent version with an _old extension since the summitservers are being shut down quickly [8fab060] do not look at this' [edf90c3] Add missing colons [bcda871] Make max_acq_iter and target_pointing_tolerance configurable [079c08c] changed hexapod code to fix some bugs [4c3d8a0] added way to set hexapod offsets back to what it was before running [2707630] Accidental misvalue [bf59a51] Fix other typo [8ec6557] Fix typo of bool [d0969e3] Fix description in the wrong place [05eda2e] Add a much needed slew for acquisition! [8889061] Add ability to do_acquire and do_take_sequence [dfc65fc] Clean a lot of cruft [447bc17] WIP 10 - big one [f14f734] Fix single character [0f628e5] WIP  something [d0ab324] WIP 9 [973aff3] WIP 8 [b58def5] WIP 7 [cf3180e] WIP 6 [507d5e4] WIP 5 [0fc5b92] add script to scripts folder for latiss_acquire_and_take_sequence [4c48f99] WIP 4 [ac4dd31] WIP 3 [eabb837] WIP 2 [ab07500] WIP 1 [66d5059] flake8 cleanup [f9b0a7e] Adding broken latiss_acquire_and_take_sequence script to pair program with Merlin [3dda1bb] add latiss_take_sequence as used at telescope, which is pure wrapping of RHLs code [d256156] updates to latiss_acquire_target.py - now should function with scriptQueue. Works on-sky [66c9b50] update latiss_acquire_target for use in scriptQueue. Also updated readme in scripts folder to indicate appropriate permissions are required [35df5e2] Add script file for latiss_acquire_target.py [31d6dd6] Initial commit of latiss_acquire_target.py, completely untested at this point [165feb2] update latiss_cwfs_align including nSigmaToGrow fix [cf8440a] left a bracket by accident [0d1a6b2] added comments in made in PR in DM-22700 that were intentionally omitted as it was afraid it would cause conflict due to two of us working on the same file simultaneously [934a86b] first commit for adding binning to latiss_cwfs_align.py

---
+++ scripts/auxtel/latiss_acquire_and_take_sequence.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+from lsst.ts.externalscripts.auxtel.latiss_acquire_and_take_sequence import LatissAcquireAndTakeSequence
+
+LatissAcquireAndTakeSequence.main()
---
+++ scripts/auxtel/latiss_take_sequence.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+from lsst.ts.externalscripts.auxtel.latiss_acquire_target import LatissTakeSequence
+
+LatissTakeSequence.main()
