ca0e29ab15da1dbf943d7dc6944b70770fdd8852
Andres A. Plazas Malagon <aplazas@astro.princeton.edu>
2021-08-19 09:58:45 -0500

    Add unit tests

---
+++ tests/maintel/test_make_comcam_calibrations.py
@@ -0,0 +1,83 @@
+# This file is part of ts_standardscripts
+#
+# Developed for the LSST Telescope and Site Systems.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+
+
+import unittest
+import logging
+
+from lsst.ts import standardscripts
+from lsst.ts import externalscripts
+from lsst.ts.externalscripts.maintel import MakeComCamCalibrations
+
+logger = logging.getLogger(__name__)
+logger.propagate = True
+
+
+class TestMakeComCamCalibrations(
+    standardscripts.BaseScriptTestCase, unittest.IsolatedAsyncioTestCase
+):
+    async def basic_make_script(self, index):
+        logger.debug("Starting basic_make_script")
+        self.script = MakeComCamCalibrations(index=index)
+
+        logger.debug("Finished initializing from basic_make_script")
+        # Return a single element tuple
+        return (self.script,)
+
+    async def test_configure(self):
+        async with self.make_script():
+
+            # Try configure with minimum set of parameters declared
+            # Note that all are scalars and should be converted to arrays
+            n_bias = 2
+            n_dark = 2
+            exp_times_dark = 10
+            n_flat = 4
+            exp_times_flat = [10, 10, 50, 50]
+            detectors = "(0, 1, 2, 3, 4, 5, 6, 7, 8)"
+            n_processes = 4
+
+            await self.configure_script(
+                n_bias=n_bias,
+                n_dark=n_dark,
+                n_flat=n_flat,
+                exp_times_dark=exp_times_dark,
+                exp_times_flat=exp_times_flat,
+                detectors=detectors,
+                n_processes=n_processes,
+            )
+
+            self.assertEqual(self.script.config.n_bias, n_bias)
+            self.assertEqual(self.script.config.n_dark, n_dark)
+            self.assertEqual(self.script.config.n_flat, n_flat)
+            self.assertEqual(self.script.config.exp_times_dark, exp_times_dark)
+            self.assertEqual(self.script.config.exp_times_flat, exp_times_flat)
+            self.assertEqual(self.script.config.n_processes, n_processes)
+            self.assertEqual(self.script.config.detectors, detectors)
+
+    async def test_executable(self):
+        scripts_dir = externalscripts.get_scripts_dir()
+        script_path = scripts_dir / "maintel" / "make_comcam_calibrations.py"
+        logger.debug(f"Checking for script in {script_path}")
+        await self.check_executable(script_path)
+
+
+if __name__ == "__main__":
+    unittest.main()
