8d4e1da535532fa6870378fe65be67691d58362c
Bruno Quint <bquint@lsst.org>
2023-05-05 11:44:42 -0400

    Run black in random_walk.py

--- python/lsst/ts/externalscripts/maintel/tma/random_walk.py
+++ python/lsst/ts/externalscripts/maintel/tma/random_walk.py
@@ -237,8 +237,8 @@ class RandomWalk(BaseTrackTarget):
             )
 
     async def random_walk_azel_by_time(self):
-        """Generate Az/El coordinates using random offsets for a finite 
-        amount of time. 
+        """Generate Az/El coordinates using random offsets for a finite
+        amount of time.
         """
         counter = 0
         timer_task = asyncio.create_task(asyncio.sleep(self.config.total_time))
