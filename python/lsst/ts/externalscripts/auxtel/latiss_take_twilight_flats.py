0d0945f21f1d0580ce20e71d102900b713ab22b4
elanaku <eurbach@g.harvard.edu>
2024-09-06 18:17:59 -0400

    Add TakeTwilightFlatsLatiss script

---
+++ python/lsst/ts/externalscripts/auxtel/latiss_take_twilight_flats.py
@@ -0,0 +1,149 @@
+# This file is part of ts_externalscripts
+#
+# Developed for the LSST Telescope and Site Systems.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+
+__all__ = ["TakeTwilightFlatsLatiss"]
+
+import yaml
+from lsst.ts.observatory.control.auxtel.atcs import ATCS
+from lsst.ts.observatory.control.auxtel.latiss import LATISS, LATISSUsages
+
+from ..base_take_twilight_flats import BaseTakeTwilightFlats
+
+
+class TakeTwilightFlatsLatiss(BaseTakeTwilightFlats):
+    """Specialized script for taking Twilight flats with LATISS."""
+
+    def __init__(self, index):
+        super().__init__(index=index, descr="Take Twilight flats with LATISS.")
+
+        self.atcs = None
+        self.latiss = None
+
+        self.instrument_name = "LATISS"
+
+    @property
+    def tcs(self):
+        return self.atcs
+
+    async def configure_tcs(self) -> None:
+        """Handle creating the ATCS object and waiting remote to start."""
+        if self.atcs is None:
+            self.log.debug("Creating ATCS.")
+            self.atcs = ATCS(
+                domain=self.domain,
+                log=self.log,
+            )
+            await self.atcs.start_task
+        else:
+            self.log.debug("ATCS already defined, skipping.")
+
+    @property
+    def camera(self):
+        return self.latiss
+
+    async def configure_camera(self) -> None:
+        """Handle creating the camera object and waiting remote to start."""
+        if self.latiss is None:
+            self.log.debug("Creating Camera.")
+            self.latiss = LATISS(
+                self.domain, intended_usage=LATISSUsages.TakeImage, log=self.log
+            )
+            await self.latiss.start_task
+        else:
+            self.log.debug("Camera already defined, skipping.")
+
+    @classmethod
+    def get_schema(cls):
+        schema_yaml = """
+            $schema: http://json-schema.org/draft-07/schema#
+            $id: https://github.com/lsst-ts/ts_externalscripts/take_twilight_flats_latiss.yaml
+            title: TakeTwilightFlatsLatiss v1
+            description: Configuration for TakeTwilightFlatsLatiss.
+            type: object
+            properties:
+              filter:
+                description: Filter name or ID.
+                anyOf:
+                  - type: string
+                  - type: integer
+                    minimum: 1
+                  - type: "null"
+                default: "SDSSr_65mm"
+              grating:
+                description: Grating name; if omitted the grating is not changed.
+                anyOf:
+                  - type: string
+                  - type: integer
+                    minimum: 1
+                  - type: "null"
+                default: null
+              linear_stage:
+                description: Linear stage position; if omitted the linear stage is not moved.
+                anyOf:
+                  - type: number
+                  - type: "null"
+                default: null
+            additionalProperties: false
+        """
+        schema_dict = yaml.safe_load(schema_yaml)
+
+        base_schema_dict = super(TakeTwilightFlatsLatiss, cls).get_schema()
+
+        for prop in base_schema_dict["properties"]:
+            schema_dict["properties"][prop] = base_schema_dict["properties"][prop]
+
+        return schema_dict
+
+    def get_sky_counts(self) -> float:
+        """Abstract method to get the median sky counts from the last image.
+
+        Returns
+        -------
+        float
+            Sky counts in electrons.
+        """
+        raise NotImplementedError()
+
+    def get_instrument_name(self) -> str:
+        """Get instrument name.
+
+        Returns
+        -------
+        instrument_name: `string`
+        """
+        return self.instrument_name
+
+    def get_instrument_configuration(self) -> dict:
+        return dict(
+            filter=self.config.filter,
+            grating=self.config.grating,
+            linear_stage=self.config.linear_stage,
+        )
+
+    def get_instrument_filter(self) -> str:
+        """Get instrument filter configuration.
+
+        Returns
+        -------
+        instrument_filter: `string`
+        """
+        filter = self.config.filter
+        grating = self.config.grating
+        return f"{filter}~{grating}"
