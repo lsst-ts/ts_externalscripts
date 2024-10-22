935a21bedc63ce6a277fb50321b0fae3115efc36
elanaku <eurbach@g.harvard.edu>
2024-10-22 19:14:11 +0000

    Update inits

--- python/lsst/ts/externalscripts/maintel/__init__.py
+++ python/lsst/ts/externalscripts/maintel/__init__.py
@@ -22,5 +22,7 @@
 from .make_comcam_calibrations import *
 from .take_comcam_guider_image import *
 from .take_ptc_flats_comcam import *
+from .take_twilight_flats_comcam import *
+from .take_twilight_flats_lsstcam import *
 from .track_target_sched import *
 from .warmup_hexapod import *
