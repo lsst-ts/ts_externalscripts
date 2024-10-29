00860bf6d1d5e2e44e3d28e75643dd7fac81383e
Tiago Ribeiro <tribeiro@lsst.org>
2024-10-28 19:52:23 -0700

    fixup! In base_parameter_march.py, wait for tracking to start to continue.

--- python/lsst/ts/externalscripts/base_parameter_march.py
+++ python/lsst/ts/externalscripts/base_parameter_march.py
@@ -359,7 +359,7 @@ class BaseParameterMarch(BaseBlockScript):
         await self.tcs.stop_tracking()
         await asyncio.sleep(1.0)
         await self.tcs.start_tracking()
-        await self.tcs.check_tracking()
+        await self.tcs.check_tracking(track_duration=1.0)
 
     async def parameter_march(self) -> None:
         """Perform the parameter_march operation."""
